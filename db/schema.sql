18297aeb01d72d859cd7c5597b4baf6394bcb92f
Daron Schmit <darons23@gmail.com>
2021-01-19 20:48:05 -0600

    adjusted schema to add History table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,12 @@
 DROP DATABASE IF EXISTS dinnerandamovie;
 -- Creates the "blogger" database --
 CREATE DATABASE dinnerandamovie;
+
+CREATE TABLE History
+(
+	id int NOT NULL AUTO_INCREMENT,
+	dinnerSearchTerm VARCHAR(255),
+	movieSearchTerm VARCHAR(255),
+	likes INT DEFAULT 0,
+	PRIMARY KEY (id)
+);
