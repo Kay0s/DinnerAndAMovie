de3a8d042ba58e03df8ade360c89fb8b3ef0eb3a
Kay0s <hamilton.kristina@gmail.com>
2021-01-20 12:44:35 -0600

    created associations for models/dinner.js and models/movie.js

--- models/history.js
+++ models/history.js
@@ -1,31 +1,14 @@
-//using bcrypt for password hashing
-const bcrypt = require("bcryptjs");
-//exporting History object
-module.exports = function (sequelize, DataTypes) {
-  let History = sequelize.define("History", {
-    //Food
-    dinnerSearchTerm: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    //password cannot be null
-    movieSearchTerm: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  });
-  return History;
-};
 // //using bcrypt for password hashing
 // const bcrypt = require("bcryptjs");
-// //exporting Movie object
-// module.exports = (sequelize, DataTypes) => {
-//   let Movie = sequelize.define("Movie", {
-//     //Movie
+// //exporting History object
+// module.exports = function (sequelize, DataTypes) {
+//   let History = sequelize.define("History", {
+//     //Food
+//     dinnerSearchTerm: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     //password cannot be null
 //     movieSearchTerm: {
 //       type: DataTypes.STRING,
 //       allowNull: false,
@@ -34,18 +17,38 @@ module.exports = function (sequelize, DataTypes) {
 //       type: DataTypes.INTEGER,
 //       defaultValue: 0,
 //     },
-//     created_at: {
-//       type: TIMESTAMP,
-//       defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-//       allowNull: false,
-//     },
 //   });
-//   Movie.associate = (models) => {
-//     Movie.hasMany(models.Dinner, {
-//       foreignKey: "created_at",
-//       as: "dinnerAndMoviePairing",
-//       onDelete: "cascade",
-//     });
-//   };
-//   return Movie;
+//   return History;
 // };
+//using bcrypt for password hashing
+const bcrypt = require("bcryptjs");
+//exporting Movie object
+module.exports = (sequelize, DataTypes) => {
+  let Movie = sequelize.define("Movie", {
+    //Movie
+    movieSearchTerm: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50],
+      },
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    created_at: {
+      type: TIMESTAMP,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      allowNull: false,
+    },
+  });
+  Movie.associate = (models) => {
+    Movie.hasMany(models.Dinner, {
+      foreignKey: "created_at",
+      as: "dinnerAndMoviePairing",
+      onDelete: "cascade",
+    });
+  };
+  return Movie;
+};
