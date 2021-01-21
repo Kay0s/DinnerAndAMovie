0c2fd92fc2c588e384b088e27dcf60884a55c6d2
Daron Schmit <darons23@gmail.com>
2021-01-20 18:02:41 -0600

    got rid of unnecessary 'fixes'

--- routes/htmlRoutes/html-routes.js
+++ routes/htmlRoutes/html-routes.js
@@ -9,25 +9,13 @@ let path = require("path");
 // Each of the below routes just handles the HTML page that the user gets sent to.
 
 // index route loads view.html
-// router.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../../public/pairings.html"));
-// });
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/pairings.html"));
+});
 
 // favorites route loads favorites.html
 router.get("/favorites", function (req, res) {
   res.sendFile(path.join(__dirname, "../../public/favorites.html"));
 });
-
-
-router.get("/", (req, res) => {
-   movie.all(data => {
-    const indexObj = {
-       movies: data
-    };
-    console.log(indexObj);
-    res.render("index", indexObj);
-  });
-});
-
 
 module.exports = router;
