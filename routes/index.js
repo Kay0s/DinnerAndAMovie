11f97efa73e431b97bef6af5fe5bdab065a81398
Kay0s <hamilton.kristina@gmail.com>
2021-01-20 19:45:42 -0600

    upated code on index.js for rendering views/main.handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,28 @@
-let router = require("express").Router();
-router.use("/api/dinner", require("./apiRoutes/dinner-routes"));
-router.use("/api/movie", require("./apiRoutes/movie-routes"));
-router.use("/", require("./htmlRoutes/html-routes"));
+// let router = require("express").Router();
+// router.use("/api/dinner", require("./apiRoutes/dinner-routes"));
+// router.use("/api/movie", require("./apiRoutes/movie-routes"));
+// router.use("/", require("./htmlRoutes/html-routes"));
+
+let express = require("express");
+let router = express.Router();
+// Dependencies
+// =============================================================
+
+// let path = require("path");
+
+// Routes
+// =============================================================
+
+// Each of the below routes just handles the HTML page that the user gets sent to.
+
+// index route loads view.html
+router.get("/", (req, res, next) => {
+  res.render("main", { layout: "main", template: "main-template" });
+});
+
+// favorites route loads favorites.html
+router.get("/favorites", function (req, res, next) {
+  res.render("favorites", { layout: "favorites", template: "favorites-template" });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,34 +5,28 @@ const express = require("express");
 // const passport = require("./config/passport");
 require("dotenv").config();
 const axios = require("axios");
+const path = require("path");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
-const { sequelize } = require("./models");
-
+const sequelize = require("./models");
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
 // Set Handlebars.
-
 let exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main", layoutsDir: path.join(__dirname, "views/layouts") }));
 app.set("view engine", "handlebars");
-
 // Requiring our routes
 // app.use(HTMLRoutes);
 app.use(require("./routes/"));
-
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
-
 module.exports = app;
