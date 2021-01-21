54f38a63fe1a4f561127b66fca83c1c4ceec717e
Kay0s <hamilton.kristina@gmail.com>
2021-01-20 22:04:58 -0600

    updated to main

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,11 @@ let router = express.Router();
 // index route loads view.html
 router.get("/", (req, res, next) => {
   res.render("index");
-
+});
 
 // favorites route loads favorites.html
-router.get("/favorites", function (req, res, next) {
+router.get("/favorites", (req, res, next) => {
   res.render("favorites");
-}
+});
+
 module.exports = router;
