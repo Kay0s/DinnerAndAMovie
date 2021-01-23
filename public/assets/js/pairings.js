b29f86beb7ce0c9f576cfe86088f33c425dbe233
Kay0s <hamilton.kristina@gmail.com>
2021-01-23 15:04:02 -0600

    linting files

--- public/assets/js/pairings.js
+++ public/assets/js/pairings.js
@@ -1,103 +1,3 @@
-<<<<<<< HEAD
-// // Make sure we wait to attach our handlers until the DOM is fully loaded.
-// $(function () {
-//   $(".change-pairingsSearched").on("click", (event) => {
-//     let id = $(this).data("id");
-//     let newFavorite = $(this).data("newFavorite");
-
-//     let newPairingsSearchedState = {
-//       favorited: newFavorite,
-//     };
-
-//     // Send the PUT request.
-//     $.ajax("/api/pairings" + id, {
-//       type: "PUT",
-//       data: newPairingsSearchedState,
-//     }).then(function () {
-//       console.log("changed favorited to", newFavorite);
-//       // Reload the page to get the updated list
-//       location.reload();
-//     });
-//   });
-
-// $("movie-title").on("click", (event) => {
-//   let id = $(this).data("id");
-//   let title = $(this).data("title");
-
-//   let newPairingsInputed = {
-//     movieTitle: title,
-//   };
-//   //Make sure to preventDefault on a submit event.
-//   //  event.preventDefault();
-//   //Get Movie title
-//   //if movie exists in db, don't write it
-//   //else POST new movie
-//   //then post new dinner with movie's id as MovieID attribute
-
-//     name: $("#npi").val().trim(),
-//   };
-//   // Send the POST request.
-//   $.ajax("/api/movie/", {
-//     type: "POST",
-//     data: newPairingsInputed, //movie title
-//   }).then(function () {
-//     console.log("created new favorite");
-//     // Reload the page to get the updated list
-//     location.reload();
-//   });
-// });
-// // function renderEmpty() {
-// //     let alertDiv = $("<div>");
-// //     alertDiv.addClass("alert alert-danger");
-// //     alertDiv.text("Check the spelling");
-// //     movieContainer.append(alertDiv);
-// //    }
-// //    renderEmpty();
-// function searchMovieGetMeal(movie) {
-//   let movieQuery = `http://www.omdbapi.com/?apikey=${movie_api_key}=${movie}`;
-//   let mealQuery = `https://www.themealdb.com/api/json/v1/${dinner_api_key}/search.php?f=${res.Title[0]}`;
-
-//   let movieObj;
-//   let mealObj;
-
-//   ajax.get(movieQuery).then((response) => {
-//     movieObj = res;
-//     console.log(movieObj);
-//   });
-//   ajax.get(mealQuery).then((response) => {
-//     mealObj = res.meals[Math.floor(Math.random() * res.meals.length)];
-//     console.log(mealObj);
-//     renderMovie(movieObj);
-//     renderDinner(mealObj);
-//   });
-// }
-// function renderMovie(movie) {
-//   $("#movieInfo").append(`<p>${movie.Title}</p>`);
-// }
-// function renderDinner(dinner) {
-//   $("#dinnerInfo").append(`<p>${dinner.strMeal}</p>`);
-// }
-
-// $(".change-pairingsSearched").on("click", (event) => {
-//   let id = $(this).data("id");
-//   let newPairingsSearched = $(this).data("newPairingsSearched");
-
-//   let newPairingsSearchedState = {
-//     favorited: newPairingsSearched,
-//   };
-
-//         function renderDinner(responseTwo) {
-//           mealObj = res.meals[Math.floor(Math.random() * res.meals.length)];
-//           $("#dinnerInfo").append(`<p>${dinner.strMeal}</p>`);
-//         }
-//       })
-//     )
-//     .catch((errors) => {
-//       // react on errors.
-//       return res.status(404).end();
-//     });
-// }
-=======
 //   //Make sure we wait to attach our handlers until the DOM is fully loaded.
 //  $(function () {
 //    $(".change-pairingsSearched").on("click", (event) => {
@@ -132,145 +32,146 @@
 // //   //   //if movie exists in db, don't write it
 // //   //   //else POST new movie
 //   //     //then post new dinner with movie's id as MovieID attribute
-    // let newPairingsInputed = {
-    //   name: $("#npi").val().trim(),
-      
-    // };
+// let newPairingsInputed = {
+//   name: $("#npi").val().trim(),
 
-    $("#search").on("click", function () {
-      searchMovieGetMeal($("#movietitle").val());
-    });
+// };
 
-    $("#doStuff").on("click", doStuff);
+$("#search").on("click", () => {
+  searchMovieGetMeal($("#movietitle").val());
+});
 
-    function searchMovieGetMeal(title) {
-      axios({
-        url: `/api/movie/pairing/${title}`,
-        method: "get",
-        response: "json",
-      }).then((response) => {
-        console.log(response); //includes a bunch of stuff you don't care about
-        console.log(response.data); //give you just the stuff you care about
-        console.log(response.data[0]); //since this call returns an array, we get the first index
-        console.log(response.data[1]); // second index
-        renderMovieAndDinner(response.data[0], response.data[1]);
-      });
-    }
+$("#doStuff").on("click", doStuff);
 
-    function renderMovieAndDinner(movie, dinner) {
-      $("#movieInfo").append(`<p>${movie.Title}</p>`);
-      let elementID = movie.Title.replace(/\s/g, '')+"+"+dinner.strMeal.replace(/\s/g, '');
-      let newDinner = $(`<p id="${elementID}">${dinner.strMeal}</p>`);
-      dinner.movieTitle = movie.Title;
-      newDinner.data("dinner", dinner);
-      newButton = $(`<button>Like</button>`);
-      newButton.on("click", () => {likeButton(movie.Title, dinner.strMeal)});
-      $(newDinner).append(newButton);
-      $("#dinnerInfo").append(newDinner);
-    }
+function searchMovieGetMeal(title) {
+  axios({
+    url: `/api/movie/pairing/${title}`,
+    method: "get",
+    response: "json",
+  }).then((response) => {
+    console.log(response); //includes a bunch of stuff you don't care about
+    console.log(response.data); //give you just the stuff you care about
+    console.log(response.data[0]); //since this call returns an array, we get the first index
+    console.log(response.data[1]); // second index
+    renderMovieAndDinner(response.data[0], response.data[1]);
+  });
+}
 
-    function doStuff() {
-      searchMovieGetMeal("big");
-    }
+function renderMovieAndDinner(movie, dinner) {
+  $("#movieInfo").append(`<p>${movie.Title}</p>`);
+  const elementID =
+    movie.Title.replace(/\s/g, "") + "+" + dinner.strMeal.replace(/\s/g, "");
+  const newDinner = $(`<p id="${elementID}">${dinner.strMeal}</p>`);
+  dinner.movieTitle = movie.Title;
+  newDinner.data("dinner", dinner);
+  newButton = $("<button>Like</button>");
+  newButton.on("click", () => {
+    likeButton(movie.Title, dinner.strMeal);
+  });
+  $(newDinner).append(newButton);
+  $("#dinnerInfo").append(newDinner);
+}
 
-    function deleteAThing(someMovieTitle) {
-      //sequelize stuff
-      axios({
-        url: `/api/movie/1`,
-        method: "get",
-        response: "json",
-      }).then((response) => {
-        console.log(response.data);
-      });
-      db.movie.destroy({
-        where: {
-          title: someMovieTitle,
-        },
-      });
-    }
-    function likeButton(movieTitle, dinnerName) {
-      console.log("checking like button");
-      //check if combo exists in db
-      //get all from db
-      axios({
-        url: "/api/movie/All",
-        method: "get",
-        response: "json"
-      }).then((response) => {
-        //console.log(response.data);
-        response.data.forEach((movie) => { //check each db entry for a matching title
-          if (movie.title === movieTitle) { //if it exists, check its dinners
-            console.log("found one: ", movie);
-            movie.Dinners.forEach((dinner) => {
-                if (dinner.mealSTR === dinnerName) { //if a dinner matches, update its likes with a put request
-                  console.log(dinner.mealSTR, dinnerName);
-                console.log("got a dinner: ", dinner);
-                axios({
-                    url: `/api/dinner/like/${movie.id}/${dinner.id}`,
-                    method: 'put',
-                    response: 'json'
-                })
-                .then(response => {
-                    console.log(`success! like added to ${movieTitle} and ${dinnerName}`);
-                    return response.data;
-                })
-              }
+function doStuff() {
+  searchMovieGetMeal("big");
+}
+
+// function deleteAThing(someMovieTitle) {
+//   console.log("deleteAThing");
+//   //sequelize stuff
+//   axios({
+//     url: `/api/movie/1`,
+//     method: "get",
+//     response: "json",
+//   }).then((response) => {
+//     console.log(response.data);
+//   });
+//   db.movie.destroy({
+//     where: {
+//       title: someMovieTitle,
+//     },
+//   });
+// }
+function likeButton(movieTitle, dinnerName) {
+  console.log("checking like button");
+  //check if combo exists in db
+  //get all from db
+  axios({
+    url: "/api/movie/All",
+    method: "get",
+    response: "json",
+  }).then((response) => {
+    //console.log(response.data);
+    response.data.forEach((movie) => {
+      //check each db entry for a matching title
+      if (movie.title === movieTitle) {
+        //if it exists, check its dinners
+        console.log("found one: ", movie);
+        movie.Dinners.forEach((dinner) => {
+          if (dinner.mealSTR === dinnerName) {
+            //if a dinner matches, update its likes with a put request
+            console.log(dinner.mealSTR, dinnerName);
+            console.log("got a dinner: ", dinner);
+            axios({
+              url: `/api/dinner/like/${movie.id}/${dinner.id}`,
+              method: "put",
+              response: "json",
+            }).then((response) => {
+              console.log(
+                `success! like added to ${movieTitle} and ${dinnerName}`
+              );
+              return response.data;
             });
           }
-          else{
-              console.log("nothing found");
-          }
         });
-        axios({
-            url:"/api/movie/",
-            method: "post",
-            data: {
-              title: movieTitle
-            },
-            response: "json"
-        })
-        .then(response => {
-          axios({
-              url:"api/dinner/",
-              method: "post",
-              data: {
-                  mealSTR: dinnerName,
-                  likes: 1,
-                  MovieId: response.data.id
-              },
-              response: "json"
-          })
-          .then(response => {
-              console.log("made a new Movie and Dinner Pairing");
-          })
-        })
+      } else {
+        console.log("nothing found");
+      }
+    });
+    axios({
+      url: "/api/movie/",
+      method: "post",
+      data: {
+        title: movieTitle,
+      },
+      response: "json",
+    }).then((response) => {
+      axios({
+        url: "api/dinner/",
+        method: "post",
+        data: {
+          mealSTR: dinnerName,
+          likes: 1,
+          MovieId: response.data.id,
+        },
+        response: "json",
+      }).then((response) => {
+        console.log(response);
+        console.log("made a new Movie and Dinner Pairing");
       });
-      //if so, put another like
-      //if not, post new Dinner
-    };
- 
+    });
+  });
+  //if so, put another like
+  //if not, post new Dinner
+}
 
 //     // Send the POST request.
-  //   $.ajax("/api/movie/", {
-  //      type: "POST",
-  //      data: newPairingsInputed, //movie title
-  //    }).then(function () {
-  //      console.log("created new favorite");
-  //      // Reload the page to get the updated list
-  //      location.reload();
-  //    });
-  //  });
-    //function renderEmpty() {
-      //  let alertDiv = $("<div>");
+//   $.ajax("/api/movie/", {
+//      type: "POST",
+//      data: newPairingsInputed, //movie title
+//    }).then(function () {
+//      console.log("created new favorite");
+//      // Reload the page to get the updated list
+//      location.reload();
+//    });
+//  });
+//function renderEmpty() {
+//  let alertDiv = $("<div>");
 //   //     alertDiv.addClass("alert alert-danger");
 //   //     alertDiv.text("Check the spelling");
 //   //     movieContainer.append(alertDiv);
 //   //    }
 //   //    renderEmpty();
-
-
-
-
 
 // // $(".change-pairingsSearched").on("click", (event) => {
 // //   let id = $(this).data("id");
@@ -301,5 +202,3 @@
 // //      res.response("Pairing added to Favorites!")
 // //     }
 // //   })
-  
->>>>>>> e84c9390b80e8d4963cda6537ad4d46aadb53b35
--- routes/htmlRoutes/html-routes.js
+++ routes/htmlRoutes/html-routes.js
@@ -1,7 +1,6 @@
 // Dependencies
 // =============================================================
 const router = require("express").Router();
-const path = require("path");
 
 // Routes
 // =============================================================
@@ -9,25 +8,15 @@ const path = require("path");
 // Each of the below routes just handles the HTML page that the user gets sent to.
 
 // index route loads view.html
-<<<<<<< HEAD
-router.get("/", (req, res, next) => {
-  res.render("index");
-=======
 router.get("/", (req, res) => {
-  res.render("index")
->>>>>>> e84c9390b80e8d4963cda6537ad4d46aadb53b35
+  res.render("index");
 });
 router.get("/homepage", (req, res) => {
-  res.render("homepage")
+  res.render("homepage");
 });
 // favorites route loads favorites.html
-<<<<<<< HEAD
-router.get("/favorites", (req, res, next) => {
+router.get("/favorites", (req, res) => {
   res.render("favorites");
-=======
-router.get("/favorites", function (req, res) {
-  res.render("favorites")
->>>>>>> e84c9390b80e8d4963cda6537ad4d46aadb53b35
 });
 
 module.exports = router;
