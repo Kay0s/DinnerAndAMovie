516e83caa5c247bd23183cc8a5ee9393ad6f8651
Kay0s <hamilton.kristina@gmail.com>
2021-01-22 23:53:14 -0600

    added get all and delete function to favorite.js

--- public/assets/js/favorites.js
+++ public/assets/js/favorites.js
@@ -1,16 +1,46 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function () {
-    $(".delete-favorite").on("click", function (event) {
-      let id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/:movies/:dinners" + id, {
-        type: "DELETE",
-      }).then(function () {
-        console.log("deleted favorite", id);
-        // Reload the page to get the updated list
-        location.reload();
+//Make sure we wait to attach our handlers until the DOM is fully loaded.
+// $(function () {
+
+
+  router.get("/All", (req, res) => {
+    db.Movie.findAll({
+      include: [db.Dinner],
+    }).then(response(db.Movie, db.Dinner)) => {
+
+      renderMovie(db.Movie);
+      renderDinner(db.Dinner);
+
+    });
+  }
+      function renderMovie(db.Movie) {
+        $("#movieInfo").append(`<p>${movie.Title}</p>`);
+      }
+      function renderDinner(db.Dinner) {
+        $("#dinnerInfo").append(`<p>${dinner.strMeal}</p>`);
+      }
+      
+
+
+      
+$(".delete-favorite").on("click", function (event) {
+  let id = $(this).data("id");
+  // Send the DELETE request.
+  router.delete("/bytitle/:title", (req, res) => {
+    //findall where title=req.params.title
+    //attributes:id
+    db.Movie.findOne({
+      where: {
+        title: req.params.title,
+      },
+      attributes: ["id"],
+    }).then((dbMovieID) => {
+      db.Movie.destroy({
+        where: {
+          id: dbMovieID.id,
+        },
+      }).then((dbMovie) => {
+        res.json(dbMovie);
       });
     });
   });
-  
+})
--- public/assets/js/pairings.js
+++ public/assets/js/pairings.js
@@ -1,83 +1,6 @@
+require("dotenv").config();
 const movie_api_key = process.env.MOVIE_API_KEY;
 const dinner_api_key = process.env.DINNER_API_KEY;
-
-// $(".change-pairingsSearched").on("click", (event) => {
-//   let id = $(this).data("id");
-//   let newPairingsSearched = $(this).data("newPairingsSearched");
-
-//   let newPairingsSearchedState = {
-//     favorited: newPairingsSearched,
-//   };
-
-//   // Send the PUT request.
-//   $.ajax("/api/pairings" + id, {
-//     type: "PUT",
-//     data: newPairingsSearchedState,
-//   }).then(() => {
-//     console.log("changed favorited to", newFavorite);
-//     // Reload the page to get the updated list
-//     location.reload();
-//   });
-// });
-
-// $(".create-form").on("submit", (event) => {
-//   // Make sure to preventDefault on a submit event.
-//   event.preventDefault();
-//   //Get Movie title
-//   //if movie exists in db, don't write it
-//   //else POST new movie
-//   //then post new dinner with movie's id as MovieID attribute
-//   let newPairingsInputed = {
-//     movie_name: $("#npi").val().trim(),
-//   };
-//   // Send the POST request.
-//   $.ajax("/api/movie/", {
-//     type: "POST",
-//     data: newPairingsInputed, //movie title
-//   }).then(function () {
-//     console.log("created new favorite");
-//     // Reload the page to get the updated list
-//     location.reload();
-//   });
-// });
-
-// // Function for handling what to render when a movie title isn't entered
-// function renderEmpty() {
-//   let alertDiv = $("<div>");
-//   alertDiv.addClass("alert alert-danger");
-//   alertDiv.text("You must type in a move name before pressing the search button.");
-//   movieContainer.append(alertDiv);
-// }
-
-// $(".change-liked").on("click", (event) => {
-//   let id = $(this).data("id");
-//   let newLiked = $(this).data("newLiked");
-
-//   let newLikedState = {
-//     favorited: newFavorite,
-//   };
-// } let Favorite = sequelize.define('Favorite', {
-//   movie_name: Sequelize.STRING,
-//   dinner_name: Sequelize.STRING,
-// }, {
-//   tableName: 'favorites',
-//   timestamps: false
-// }));
-
-// Favorites
-//   .create({
-//     movie_name: movie_name,
-//     dinner_name: dinner_name
-
-//   })
-//   .complete((err, favorite) => {
-//     if (err) {
-//      console.log(err);
-//      res.response("Error adding pairing to Favorites")
-//     } else {
-//      res.response("Pairing added to Favorites!")
-//     }
-//   })
 
 $("#submit").on("click", (event) => {
   searchMovieGetMeal($("#movie_name").val());
@@ -110,6 +33,84 @@ function renderDinner(dinner) {
 
 function renderDinner(dinner) {
   $("#dinnerInfo").append(`<p>${dinner.strMeal}</p>`);
+
+  // $(".change-pairingsSearched").on("click", (event) => {
+  //   let id = $(this).data("id");
+  //   let newPairingsSearched = $(this).data("newPairingsSearched");
+
+  //   let newPairingsSearchedState = {
+  //     favorited: newPairingsSearched,
+  //   };
+
+  //   // Send the PUT request.
+  //   $.ajax("/api/pairings" + id, {
+  //     type: "PUT",
+  //     data: newPairingsSearchedState,
+  //   }).then(() => {
+  //     console.log("changed favorited to", newFavorite);
+  //     // Reload the page to get the updated list
+  //     location.reload();
+  //   });
+  // });
+
+  // $(".create-form").on("submit", (event) => {
+  //   // Make sure to preventDefault on a submit event.
+  //   event.preventDefault();
+  //   //Get Movie title
+  //   //if movie exists in db, don't write it
+  //   //else POST new movie
+  //   //then post new dinner with movie's id as MovieID attribute
+  //   let newPairingsInputed = {
+  //     movie_name: $("#npi").val().trim(),
+  //   };
+  //   // Send the POST request.
+  //   $.ajax("/api/movie/", {
+  //     type: "POST",
+  //     data: newPairingsInputed, //movie title
+  //   }).then(function () {
+  //     console.log("created new favorite");
+  //     // Reload the page to get the updated list
+  //     location.reload();
+  //   });
+  // });
+
+  // // Function for handling what to render when a movie title isn't entered
+  // function renderEmpty() {
+  //   let alertDiv = $("<div>");
+  //   alertDiv.addClass("alert alert-danger");
+  //   alertDiv.text("You must type in a move name before pressing the search button.");
+  //   movieContainer.append(alertDiv);
+  // }
+
+  // $(".change-liked").on("click", (event) => {
+  //   let id = $(this).data("id");
+  //   let newLiked = $(this).data("newLiked");
+
+  //   let newLikedState = {
+  //     favorited: newFavorite,
+  //   };
+  // } let Favorite = sequelize.define('Favorite', {
+  //   movie_name: Sequelize.STRING,
+  //   dinner_name: Sequelize.STRING,
+  // }, {
+  //   tableName: 'favorites',
+  //   timestamps: false
+  // }));
+
+  // Favorites
+  //   .create({
+  //     movie_name: movie_name,
+  //     dinner_name: dinner_name
+
+  //   })
+  //   .complete((err, favorite) => {
+  //     if (err) {
+  //      console.log(err);
+  //      res.response("Error adding pairing to Favorites")
+  //     } else {
+  //      res.response("Pairing added to Favorites!")
+  //     }
+  //   })
 }
 //   $.ajax({
 //     url: `http://www.omdbapi.com/?apikey=${movie_api_key}=${movie}`,
