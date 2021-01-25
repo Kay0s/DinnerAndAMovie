235f438bf88066b8067873965a80b6d08f0f5ec6
Alice Piar <adpir30@gmail.com>
2021-01-24 21:51:43 -0400

    trying to fix delete button

--- public/assets/js/favorites.js
+++ public/assets/js/favorites.js
@@ -1,5 +1,5 @@
 //Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(() => {
+$(function(){
   $.get("/api/dinner/All", (resp) => {
     console.log("All",resp);
     renderMovie(resp.movie);
@@ -11,7 +11,7 @@ $(() => {
   function renderDinner(dinner) {
     $("#dinnerInfo").append(`<p>${dinner.strMeal}</p>`);
   }
-});
+
 // axios.get("/All", (req, res) => {
 //   db.Movie.findAll({
 //     include: [db.Dinner],
@@ -27,29 +27,42 @@ $(() => {
 //RenderMovieAndDinners
 
 
+$("#delete-favorite").on("click", function(event) {
+  let id = $(this).data("id");
 
-
- $("#delete-favorite").on("click", function (event) {
-   const id = $(this).data("id");
- console.log("hello");
-    console.log(id, event);
-    //Send the DELETE request.
-   $.delete("/bytitle/:title", (req, res) => {
-     findall where title=req.params.title
-     //attributes:id
-     db.Movie.findOne({
-       where: {
-         title: req.params.title,
-       },
-       attributes: ["id"],
-     }).then((dbMovieID) => {
-       db.Movie.destroy({
-         where: {
-           id: dbMovieID.id,
-         },
-       }).then((dbMovie) => {
-         res.json(dbMovie);
-       });
-     });
-   });
- });
+  // Send the DELETE request.
+  $.ajax("/api/dinner/" + id, {
+    type: "DELETE"
+  }).then(
+    function() {
+      console.log("deleted dinner", id);
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
+});
+});
+//  $("#delete-favorite").on("click", function (event) {
+//    const id = $(this).data("id");
+//  console.log("hello");
+//     console.log(id, event);
+//     //Send the DELETE request.
+//    $.delete("/bytitle/:title", (req, res) => {
+//      //findall where title=req.params.title
+//      //attributes:id
+//      db.Movie.findOne({
+//        where: {
+//          title: req.params.title,
+//        },
+//        attributes: ["id"],
+//      }).then((dbMovieID) => {
+//        db.Movie.destroy({
+//          where: {
+//            id: dbMovieID.id,
+//          },
+//        }).then((dbMovie) => {
+//          res.json(dbMovie);
+//        });
+//      });
+//    });
+//  });
